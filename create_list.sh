9b75b0d20121047aebe63f5bd80b67535e395c05
Chetak Sasalu Marulasiddappa <cmarulas@redhat.com>
2023-03-09 11:17:27 +0100

    Update url of fake site list

--- create_list.sh
+++ create_list.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-list=$(curl -q https://www.konspiratori.sk/assets/downloads/zoznam.txt | cut -d',' -f1)
+list=$(curl -q  https://www.konspiratori.sk/static/lists/zoznam.txt | cut -d',' -f1)
 echo "#fakenews-cz-sk" > hosts
 for hostname in ${list}; do
     echo "0.0.0.0 ${hostname}" >> hosts
